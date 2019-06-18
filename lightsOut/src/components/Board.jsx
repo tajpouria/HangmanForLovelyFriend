bfe4f6b79bed368d780082dbcf66900528c19b1c
tajpouria <charliswager@gmail.com>
2019-06-18 13:46:45 +0430

    lightouts out added

--- lightsOut/src/components/Board.jsx
+++ lightsOut/src/components/Board.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import Cell from './Cell';
 
@@ -14,6 +15,7 @@ export default class Board extends Component {
 
     this.state = {
       cells: [],
+      isWinner: false,
     };
 
     this.cellClicked = this.cellClicked.bind(this);
@@ -54,19 +56,27 @@ export default class Board extends Component {
 
   cellClicked(coords) {
     let [row, col] = coords.split('-');
-    row = parseInt(row);
-    col = parseInt(col);
+    row = parseInt(row, 10);
+    col = parseInt(col, 10);
 
-    const newCells = [...this.state.cells];
+    const { cells } = this.state;
+    const newCells = [...cells];
+
     newCells[row][col] = !newCells[row][col];
-    newCells[row + 1][col] = !newCells[row + 1][col];
-    newCells[row - 1][col] = !newCells[row - 1][col];
-    newCells[row][col + 1] = !newCells[row][col + 1];
-    newCells[row][col - 1] = !newCells[row][col - 1];
-
-    console.log(newCells);
+    if (row > 0) newCells[row - 1][col] = !newCells[row - 1][col];
+    if (col > 0) newCells[row][col - 1] = !newCells[row][col - 1];
+    if (row < newCells.length - 1) newCells[row + 1][col] = !newCells[row + 1][col];
+    if (col < newCells[row].length - 1) newCells[row][col + 1] = !newCells[row][col + 1];
 
     this.setState({ cells: newCells });
+
+    for (let i = 0; i < cells.length; i++) {
+      for (let j = 0; j < cells[i].length; j++) {
+        if (cells[i][j]) return;
+      }
+    }
+
+    this.setState({ isWinner: true });
   }
 
   renderBoard(row, idx) {
@@ -86,15 +96,33 @@ export default class Board extends Component {
   }
 
   render() {
-    const { cells } = this.state;
-    return (
-      <table className="Board">
-        <tbody>
-          {cells.map((row, idx) => (
-            <tr key={Math.random()}>{this.renderBoard(row, idx)}</tr>
-          ))}
-        </tbody>
-      </table>
+    const { cells, isWinner } = this.state;
+
+    return isWinner ? (
+      <div className="Board-title">
+        <h2 className="Board-neon">YOU</h2>
+        <h2 className="Board-flux">WIN!</h2>
+      </div>
+    ) : (
+      <div className="Board">
+        <div className="Board-title">
+          <h2 className="Board-neon">LIGHTS</h2>
+          <h2 className="Board-flux">OUT</h2>
+        </div>
+        <table className="Board-table">
+          <tbody>
+            {cells.map((row, idx) => (
+              <tr key={Math.random()}>{this.renderBoard(row, idx)}</tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
+
+Board.propTypes = {
+  nCol: PropTypes.number,
+  nRow: PropTypes.number,
+  lightsOnChance: PropTypes.number,
+};
